0f508668fa61463324d2c0f2dc05d8fa13c21c95
Aryan Kumar <kumar.aryan.1303@gmail.com>
2026-02-20 11:12:44 +0530

    Initial Spring Boot Project setup with docker and basic security

---
+++ src/test/java/com/aryan/springboot/leavemanagement/LeavemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryan.springboot.leavemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeavemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
